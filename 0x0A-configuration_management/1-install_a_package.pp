e41456bf6c8446354e58705c48a413d0f1d9d6e8
root <root@CSN.localdomain>
2024-04-20 00:35:24 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,13 +2,13 @@
 
 $package = 'flask'
 package { $package:
-    ensure      => '2.1.0',
-    provider    => 'pip3',
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
 
 
 $package = 'werkzeug'
 package { $package:
-    ensure      => '2.1.1',
-    provider    => 'pip3',
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
