ce868024c63d4947fb75d0dfa46357060deb759c
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-22 01:14:28 +0100

    client configuration with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,28 +1,28 @@
 # Configuration file to login without PasswordAuthentication
 
 # Ensure the SSH configuration file exists
-file { '/home/vagrant/.ssh/config':
+file { '/home/ubuntu/.ssh/config':
   ensure  => file,
-  owner   => 'vagrant',
-  group   => 'vagrant',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
   mode    => '0600',
-  require => File['/home/vagrant/.ssh'],
+  require => File['/home/ubuntu/.ssh'],
 }
 
 # Disable password authentication in the SSH client configuration
 file_line { 'Turn off passwd auth':
-  path    => '/home/vagrant/.ssh/config',
+  path    => '/home/ubuntu/.ssh/config',
   line    => 'PasswordAuthentication no',
   match   => '^PasswordAuthentication',
   replace => true,
-  require => File['/home/vagrant/.ssh/config'],
+  require => File['/home/ubuntu/.ssh/config'],
 }
 
 # Declare the identity file in the SSH client configuration
 file_line { 'Declare identity file':
-  path    => '/home/vagrant/.ssh/config',
+  path    => '/home/ubuntu/.ssh/config',
   line    => 'IdentityFile ~/.ssh/school',
   match   => '^IdentityFile',
   replace => true,
-  require => File['/home/vagrant/.ssh/config'],
+  require => File['/home/ubuntu/.ssh/config'],
 }
