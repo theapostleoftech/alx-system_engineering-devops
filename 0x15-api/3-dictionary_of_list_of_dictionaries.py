e6e450860c4691a7bf407389104c22efc8557238
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-21 05:11:55 +0100

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+"""
+A script to export data in json format
+"""
+
+import json
+import requests
+
+
+def get_data_from_api():
+    employee_url = f"https://jsonplaceholder.typicode.com/users/"
+    employee_response = requests.get(employee_url)
+    employees = employee_response.json()
+    response = {}
+
+    for employee in employees:
+        employee_id = employee.get('id')
+        todo_url = employee_url + "/{}/todos".format(employee_id)
+        todos_response = requests.get(todo_url)
+        todos = todos_response.json()
+
+        employee_username = employee.get('username')
+
+        todo_data = []
+
+        for task in todos:
+            todo = {}
+            todo["username"] = str(employee_username)
+            todo["completed"] = task.get("completed")
+            todo["task"] = str(task.get("title"))
+            todo_data.append(todo)
+
+        response[employee_id] = todo_data
+
+    return response
+
+
+def export_to_json(todo_data):
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(todo_data, json_file, indent=2)
+
+
+if __name__ == "__main__":
+    todo_data = get_data_from_api()
+    export_to_json(todo_data)
