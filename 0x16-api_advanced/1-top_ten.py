170b9d80ce2bcb74ed1928007065f519c78a8ca1
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-06-05 00:39:08 +0100

    How many subs?

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,16 +8,16 @@ from sys import argv
 
 def top_ten(subreddit):
     """Get the 10 host posts"""
-    url_sred_inf = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    url_sred_inf += "?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url += "?limit=10"
     headers = {'user-agent': 'request'}
-    response = requests.get(url_sred_inf, headers=headers,
+    response = requests.get(url, headers=headers,
                             allow_redirects=False)
     if str(response) != "<Response [200]>":
         print(None)
         return
-    r_json = response.json()
-    hot_posts_json = r_json.get("data").get("children")
+    result = response.json()
+    hot_posts_json = result.get("data").get("children")
     top_10_posts = ""
     for post in hot_posts_json:
         top_10_posts += post.get("data").get("title") + "\n"
