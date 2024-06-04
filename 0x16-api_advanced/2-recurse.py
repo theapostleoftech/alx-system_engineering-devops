2871d0203e855c27b69fca46e63bb2bb4b021f85
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-06-05 00:25:18 +0100

    Recurse it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """
-    Uses the reddit API for get the all hot posts
+    Retrieves the hot posts from the reddit API
 """
 import requests
 from sys import argv
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """Get the all host posts"""
+    """Get the all hot posts"""
     if after is None:
         return []
 
-    url_sred_inf = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    url_sred_inf += "?limit=100&after={}".format(after)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url += "?limit=100&after={}".format(after)
     headers = {'user-agent': 'request'}
-    response = requests.get(url_sred_inf, headers=headers,
+    response = requests.get(url, headers=headers,
                             allow_redirects=False)
     if str(response) != "<Response [200]>":
         return None
-    r_json = response.json()
-    hot_posts_json = r_json.get("data").get("children")
+    result = response.json()
+    hot_posts_json = result.get("data").get("children")
     for post in hot_posts_json:
         hot_list.append(post.get("data").get("title"))
-    return hot_list + recurse(subreddit, [], r_json.get("data").get("after"))
+    return hot_list + recurse(subreddit, [], result.get("data").get("after"))
