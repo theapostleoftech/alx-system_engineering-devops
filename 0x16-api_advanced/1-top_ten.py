e3490701e77024f1726c751b10a53ac6c83e5b6c
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-06-05 00:16:16 +0100

    Top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Retrieves 10 hot posts from the reddit API
+"""
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    """Get the 10 hot posts"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url += "?limit=10"
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False)
+    if str(response) != "<Response [200]>":
+        print(None)
+        return
+    result = response.json()
+    hot_posts_json = result.get("data").get("children")
+    top_10_posts = ""
+    for post in hot_posts_json:
+        top_10_posts += post.get("data").get("title") + "\n"
+    print(top_10_posts, end="")
+    
