1a1c626785afbb71ebad2eec843f9bad154fe0ac
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-06-05 00:11:03 +0100

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    This prints the number of subscribers of a subreddit
+"""
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """
+    This function gets the number of
+    subscribers of a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False)
+    if str(response) != "<Response [200]>":
+        return 0
+    result = response.json()
+    no_of_subscribers = result.get("data").get("subscribers")
+    return no_of_subscribers
